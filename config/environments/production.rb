5264df3e2a7fcc2fa744e935092d31838013424c
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-06-15 19:26:04 +0200

    devise install and config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "xpenses_production"
 
   config.action_mailer.perform_caching = false
+  
+  #Devise Mailer Config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
