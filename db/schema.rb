945c2ff5197a7f5f6ad2490d899cf73964e38070
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-06-17 14:47:47 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_171436) do
+ActiveRecord::Schema.define(version: 2021_06_17_114225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "ledger_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ledger_id"], name: "index_accounts_on_ledger_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "ledger_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ledger_id"], name: "index_categories_on_ledger_id"
+  end
+
+  create_table "ledgers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ledgers_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "ledger_id", null: false
+    t.bigint "account_id", null: false
+    t.date "date"
+    t.string "info"
+    t.bigint "subcategory_id", null: false
+    t.boolean "cleared"
+    t.decimal "payment"
+    t.decimal "deposit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+    t.index ["ledger_id"], name: "index_transactions_on_ledger_id"
+    t.index ["subcategory_id"], name: "index_transactions_on_subcategory_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +72,17 @@ ActiveRecord::Schema.define(version: 2021_06_15_171436) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "ledgers"
+  add_foreign_key "categories", "ledgers"
+  add_foreign_key "ledgers", "users"
+  add_foreign_key "subcategories", "categories"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "ledgers"
+  add_foreign_key "transactions", "subcategories"
 end
