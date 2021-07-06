0a440a8e46e84a185e972b8845ba15daf7aee3e2
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-07-06 20:15:30 +0200

    ledger basic actions

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -1,2 +1,29 @@
 class LedgersController < ApplicationController
+  def index
+    @ledgers = Ledger.all
+  end
+
+  def show
+    @ledger = Ledger.find(params[:id])
+  end
+
+  def new
+    @ledger = Ledger.new
+  end
+
+  def create
+    @ledger = Ledger.new(ledger_params)
+    @ledger.user = current_user
+    if @ledger.save!
+      redirect_to ledger_path(@ledger)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ledger_params
+    params.require(:ledger).permit(:name, :description)
+  end
 end
