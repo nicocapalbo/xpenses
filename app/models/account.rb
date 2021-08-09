61e4be4c85b06717e035501a33d65b690eb7604a
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-09 15:39:53 +0200

    accounts index cleared+balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,13 @@ class Account < ApplicationRecord
   belongs_to :ledger
   has_many :transactions
 
-  def net_account
+  def cleared_account
+    payment_totals = self.ledger.transactions.where(account: self).sum(:payment)
+    deposit_totals = self.ledger.transactions.where(account: self).sum(:deposit)
+    (deposit_totals - payment_totals)
+  end
+
+  def balanced_account
     payment_totals = self.ledger.transactions.where(account: self, cleared: true).sum(:payment)
     deposit_totals = self.ledger.transactions.where(account: self, cleared: true).sum(:deposit)
     (deposit_totals - payment_totals)
