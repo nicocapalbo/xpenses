8e3f5491d917e760c9c59dca668cc0553f52c102
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-09 21:00:54 +0200

    dashboard controller, routes rearrange, links fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
-  resources :ledgers do
+  root to: "ledgers#index"
+  resources :ledgers, except: [:index] do
+    resources :dashboard
     resources :accounts
-    
+    resources :transactions
   end
   resources :accounts
   resources :transactions
+  resources :dashboards
 end
