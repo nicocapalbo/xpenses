a32af5e5291e97bc6914b91b814e082f7b1a5093
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-10 00:16:11 +0200

    fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,13 @@
 class DashboardController < ApplicationController
+  before_action :set_ledger, only: [:index, :create]
+  
+  def index
+    @accounts = Account.where(ledger: @ledger)
+  end
+
+  private
+
+  def set_ledger
+    @ledger = Ledger.find_by(id: params[:ledger_id])
+  end
 end
