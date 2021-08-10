3f377af654073bc198cd205c521fceda761ad892
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-10 12:39:19 +0200

    updated ledger routes, removed LedgerController show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
   root to: "ledgers#index"
-  resources :ledgers, except: [:index] do
+  resources :ledgers, except: [:index, :show] do
     resources :dashboard
     resources :accounts
     resources :transactions
