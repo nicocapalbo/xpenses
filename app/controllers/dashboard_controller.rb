074acd5acb79ca32ea5cbc7df51955c6b450259a
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-10 16:44:50 +0200

    category total calculator method and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,15 @@
 class DashboardController < ApplicationController
   before_action :set_ledger, only: [:index, :create]
-  
+
   def index
     @accounts = Account.where(ledger: @ledger)
+    @categories = []
+    Category.all.each do |category|
+      @categories << {
+        category_name: category.name,
+        category_total: category.category_total
+      }
+    end
   end
 
   private
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   belongs_to :ledger
   has_many :subcategories
   has_many :transactions, through: :subcategories
+
+  def category_total
+    Subcategory.where(category: self).joins(:transactions).where('date >= ? and date <= ?', Time.now.beginning_of_month, Time.now.end_of_month).sum(:payment)
+  end
 end
