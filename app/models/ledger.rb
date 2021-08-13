98858510a8a3342490d223059c44fe645a4f427e
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-13 15:18:16 +0200

    rotating card

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -4,4 +4,13 @@ class Ledger < ApplicationRecord
   has_many :categories
   has_many :subcategories, through: :categories
   has_many :transactions
+
+  def networth_cleared
+    self.transactions.where(cleared: true).sum(:deposit) - self.transactions.where(cleared: true).sum(:payment)
+  end
+
+  def networth_balance
+    self.transactions.sum(:deposit) - self.transactions.sum(:payment)
+  end
+
 end
