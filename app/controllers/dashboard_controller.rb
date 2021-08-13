9ec1bf9d568ea0ef19f716505e8403c32253f1b5
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-13 17:21:37 +0200

    methods totals calculations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,16 +3,28 @@ class DashboardController < ApplicationController
 
   def index
     @accounts = Account.where(ledger: @ledger)
-    @categories = []
+    @categories = categories_total
+    @month_total = month_total
+  end
+
+  private
+
+  def month_total
+    month_total = 0
+    Category.all.each { |category| month_total += category.category_total }
+    month_total
+  end
+
+  def categories_total
+    categories = []
     Category.all.each do |category|
-      @categories << {
+      categories << {
         category_name: category.name,
         category_total: category.category_total
       }
     end
+    categories
   end
-
-  private
 
   def set_ledger
     @ledger = Ledger.find_by(id: params[:ledger_id])
