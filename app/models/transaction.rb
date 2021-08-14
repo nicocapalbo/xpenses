4d6cdc430be3af85332dcf9960f8687ef6abd21a
nicolas capalbo <capalbo.nicolas@gmail.com>
2021-08-14 18:57:28 +0200

    transaction method fix

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,6 @@ class Transaction < ApplicationRecord
   belongs_to :subcategory
 
   def self.month_top_transactions
-    Transaction.where('date >= ? and date <= ?', Time.now.beginning_of_month, Time.now.end_of_month).order(payment: :asc).first(5)
+    Transaction.where('date >= ? and date <= ?', Time.now.beginning_of_month, Time.now.end_of_month).where.not(payment: nil).order(payment: :desc).first(5)
   end
 end
